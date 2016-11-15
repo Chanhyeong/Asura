0b7b2bb9119bd305b6f61197be76063582c93912
highalps <highalps93@gmail.com>
2016-11-15 22:02:02 +0900

    mongoDB ok

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var mongoose  = require('mongoose');
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
 app.set('views','/views');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('./views/index2.html')
+  res.render('index')
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose  = require('mongoose');
+var Schema = mongoose.Schema;
 /* GET users listing. */
+
+mongoose.connect('mongodb://52.78.103.203:27017/test');
+var db = mongoose.connection;
+var IDschema = mongoose.Schema({
+  id: String,
+  password: String
+});
+var ID = mongoose.model('ID',IDschema);
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    var id = req.query.id;
+    var pass = req.query.password;
+    var user = new ID({id : id, password : pass});
+    user.save(function(err,user){
+      if(err)console.log("error");
+      else res.send("OK");
+    });
+
 });
 
 module.exports = router;
